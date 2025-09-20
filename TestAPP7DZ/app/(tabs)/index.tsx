6d41164c635c9de5239b09a91b0063b91ba0f586
Manuel de Jesús Gamboa Garay <manueljgg2004@gmail.com>
2025-09-20 15:21:51 -0600

    arreglo v2

--- TestAPP7DZ/app/(tabs)/index.tsx
+++ TestAPP7DZ/app/(tabs)/index.tsx
@@ -36,6 +36,7 @@ export default function HomeScreen() {
             <ParallaxScrollView headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }} headerImage={<Image source={require("@/assets/images/partial-react-logo.png")} style={styles.reactLogo} />}>
                 <ThemedView style={styles.titleContainer}>
                     <ThemedText type="title">MANUELIYO</ThemedText>
+                    <ThemedText type="title">MANUELIYO</ThemedText>
                     <Text>MI PRIMERA APLICACIÓN MÓVIL</Text>
                     <Text>ESTA ES UNA PRUEBA DE GIT PERSONAL de MANUEL</Text>
                     <HelloWave />
