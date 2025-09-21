8b1c4ad48b54e3547df7154bfd205ae21baf173a
estefanialuna11 <20233l001157@utcv.edu.mx>
2025-09-20 18:16:09 -0600

    estilos home

--- TestAPP7DZ/app/(tabs)/index.tsx
+++ TestAPP7DZ/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View, Text } from 'react-native';
 import { useState } from 'react';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
@@ -17,9 +17,18 @@ export default function HomeScreen() {
           style={{ width: '100%', height: '100%' }}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
 
-        <ThemedText type="title">Bienvenido a la tiendita xd :D</ThemedText>
+      <ThemedView style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: 8, marginBottom: 16 }}>
+        <ThemedText type="title" style={{flex: 1}}>¡Busca tu mojarra ideal!</ThemedText>
+        <View style={{ alignItems: 'center', gap: 4 }}>
+          <Image
+            source={require('@/assets/images/carrito_mojarra.jpg')}
+            style={{ width: 64, height: 64, borderRadius: 8 }}
+          />
+          <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#b78c00ff' }}>
+            Carrito
+          </Text>
+        </View>
       </ThemedView>
 
       <View>
@@ -27,40 +36,56 @@ export default function HomeScreen() {
           placeholder="Buscar..."
           value={search}
           onChangeText={setSearch}
-          style={{ marginBottom: 16, padding: 8, borderWidth: 1, borderColor: '#ccc', borderRadius: 8 }}
-        />
-
-      </View>
-
-      <View >
-        <Image
-          source={require('@/assets/images/mojarra_fit.jpg')}
-        />
-        <Image
-          source={require('@/assets/images/mojarra_frita.jpeg')}
-        />
-        <Image
-          source={require('@/assets/images/mojarra_naranja.jpeg')}
-        />
-        <Image
-          source={require('@/assets/images/mojarra_solla.png')}
+          style={{ marginTop: 16, marginBottom: 16, padding: 8, borderWidth: 1, backgroundColor: '#fff', borderColor: '#ccc', borderRadius: 8 }}
         />
       </View>
 
-
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap', gap: 32, justifyContent: 'center' }}>
+        <View style={{ alignItems: 'center' }}>
+          <Image
+        source={require('@/assets/images/mojarra_fit.jpg')}
+        style={{ width: 256, height: 256, borderRadius: 8 }}
+          />
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginTop: 8, color: '#b78c00ff' }}>
+        $120.00
+          </Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Image
+        source={require('@/assets/images/mojarra_frita.jpeg')}
+        style={{ width: 256, height: 256, borderRadius: 8 }}
+          />
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginTop: 8, color: '#b78c00ff' }}>
+        $150.00
+          </Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Image
+        source={require('@/assets/images/mojarra_naranja.jpeg')}
+        style={{ width: 256, height: 256, borderRadius: 8 }}
+          />
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginTop: 8, color: '#b78c00ff' }}>
+        $135.00
+          </Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Image
+        source={require('@/assets/images/mojarra_solla.png')}
+        style={{ width: 256, height: 256, borderRadius: 8 }}
+          />
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginTop: 8, color: '#b78c00ff' }}>
+        $140.00
+          </Text>
+        </View>
+      </View>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
   },
-  
+
 });
