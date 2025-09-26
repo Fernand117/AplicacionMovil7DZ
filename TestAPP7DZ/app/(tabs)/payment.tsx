41f4f0caad16ce1fddc46eb2c4745f8032ce34ae
xochitl roque <FatimaRoque>
2025-09-25 20:30:48 -0600

    imagenes de tarjetas

--- TestAPP7DZ/app/(tabs)/payment.tsx
+++ TestAPP7DZ/app/(tabs)/payment.tsx
@@ -1,19 +1,38 @@
-import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text>Confirmación de pedido</Text>
+        <Text>Carrito de compras</Text>
+        <Text>CP </Text>
       </View>
-      <View style={styles.subHeader}>
-        <Text> Método de pago</Text>
 
+      <View style={styles.subHeader}>
+        <Text>Datos de pago</Text>
+      </View>
+
+      <View style={styles.amount}>
+        <Text>Monto a pagar</Text>
+        <Text>$ 100.00</Text>
+        <Text>IVA incluido</Text>
       </View>
 
       <ScrollView>
-// aqui va todo xd
-<Text> idk </Text>
+        <View style={styles.imageRow}>
+          <Image source={require('../../assets/images/visa.png')}
+          style={styles.logo}
+          resizeMode='contain'
+          />
+          <Image source={require('../../assets/images/mastercard.png')}
+          style={styles.logo}
+          resizeMode='contain'
+        />
+          <Image source={require('../../assets/images/paypal.png')}
+          style={styles.logo}
+          resizeMode='contain'
+        />
+        </View>
 
       </ScrollView>
     </View>
@@ -24,13 +43,37 @@ const styles = StyleSheet.create({
   container : {
 
   },
+
   header: {
-    backgroundColor : "blue",
-    height : 100,
+    backgroundColor : "yellow",
+    height : 40,
+    justifyContent : "center",
+    paddingLeft : 5
   },
+
   subHeader : {
     backgroundColor : "white",
-    height : 40
+    height : 35,
+    justifyContent : "center",
+    paddingLeft : 5
   },
-  
+
+  amount : {
+    backgroundColor : "yellow",
+    height : 60,
+    justifyContent : "center",
+    alignItems : "center"
+  },
+
+  imageRow : {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10
+  },
+
+  logo: {
+    width: 60,
+    height: 30,
+    marginRight: 10
+  }
 });
